cce5e00fa565b2873377af3dde72e431f3b85635
Mateus Diaz <diazmateus@hotmail.com>
2020-04-21 18:09:35 -0300

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import {ReactQueryDevtools} from "react-query-devtools"
+import {useQuery} from "react-query"
 
-function App() {
-  return (
-    <div className="App">
-      teste
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <Exchange/>
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </div>
+    );
 }
 
-export default App;
+
+const fetchExchange = async currency => {
+    const response = await fetch(`https://appi.ratesapi.io/api/latest?base=${currency}`)
+    return await response.json();
+}
+
+function Exchange() {
+    const [currency, setCurrency] = useState("BRL")
+    const {status, data, error} = useQuery(currency, fetchExchange, {refetchAllOnWindowFocus: false})
+
+    if (status === "loading") return <div>Loading...</div>
+    if (status === "error") return <div>Error!</div>
+
+    return <div style={{padding: 16}}>
+        <button onClick={() => setCurrency("BRL")}>BRL</button>
+        <button onClick={() => setCurrency("USD")}>USD</button>
+        <button onClick={() => setCurrency("EUR")}>EUR</button>
+
+        <h2>DATA</h2>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+}
